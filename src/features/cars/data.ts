543780c18804b4aff66b8d0814436ae87dded023
REL <rel.yesodot@gmail.com>
2026-06-01 23:00:01 +0300

    refactor: add Car type definition to carRowData in data.ts

--- src/features/cars/data.ts
+++ src/features/cars/data.ts
@@ -1,4 +1,6 @@
-export const carRowData = [
+import type { Car } from "./types";
+
+export const carRowData: Car[] = [
     { make: "Tesla", model: "Model Y", price: 64950, electric: true },
     { make: "Ford", model: "F-Series", price: 33850, electric: false },
     { make: "Toyota", model: "Corolla", price: 29600, electric: false },
