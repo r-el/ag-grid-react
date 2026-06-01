cd8db28cb4440514823a8a4f82f8fe7ae6588651
REL <rel.yesodot@gmail.com>
2026-06-01 14:58:58 +0300

    refactor: move carRowData to a separate data file and update App component to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { AllCommunityModule } from "ag-grid-community";
-import { AgGridProvider, AgGridReact } from "ag-grid-react";
+import { AgGridProvider } from "ag-grid-react";
 import { useState } from "react";
 import "./App.css";
 import DataGrid from "./components/DataGrid";
+import { carRowData } from "./data";
 
 const modules = [AllCommunityModule];
 
 function App() {
     // Row Data: The data to be displayed.
-    const [rowData, sRowData] = useState([
-        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-        { make: "Ford", model: "F-Series", price: 33850, electric: false },
-        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-    ]);
+    const [rowData, setRowData] = useState(carRowData);
 
     // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState([
@@ -27,7 +24,7 @@ function App() {
     return (
         <>
             <AgGridProvider modules={modules}>
-                    <DataGrid rowData={rowData} columnDefs={colDefs} />
+                <DataGrid rowData={rowData} columnDefs={colDefs} />
             </AgGridProvider>
         </>
     );
---
+++ src/data.ts
@@ -0,0 +1,5 @@
+export const carRowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+];
