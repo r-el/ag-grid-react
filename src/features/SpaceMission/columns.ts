adfe29a86ea17c8eec4cb1d49a773002c48c241d
REL <rel.yesodot@gmail.com>
2026-06-03 14:56:15 +0300

    refactor: extract currency formatter for price column in spaceMissionColumnDefs

--- src/features/SpaceMission/columns.ts
+++ src/features/SpaceMission/columns.ts
@@ -1,6 +1,8 @@
 import type { ColDef } from "ag-grid-community";
 import type { SpaceMission } from "./types";
 
+const currencyFormatter = (params) => "£" + params.value.toLocaleString();
+
 export const spaceMissionColumnDefs: ColDef<SpaceMission>[] = [
     { field: "mission", filter: false }, // Column Definitions take precedence over Default Column Definitions
     { field: "company" },
@@ -9,9 +11,7 @@ export const spaceMissionColumnDefs: ColDef<SpaceMission>[] = [
     {
         field: "price",
         // Return a formatted string for this column
-        valueFormatter: (params) => {
-            return "£" + params.value.toLocaleString();
-        },
+        valueFormatter: currencyFormatter,
     },
     { field: "successful" },
     { field: "rocket" },
