0dca17aaef90b0335b55844425f066f4a92d3c9a
REL <rel.yesodot@gmail.com>
2026-06-03 13:57:18 +0300

    feat: add filter to mission column in SpaceMission component

--- src/features/SpaceMission/columns.ts
+++ src/features/SpaceMission/columns.ts
@@ -2,7 +2,7 @@ import type { ColDef } from "ag-grid-community";
 import type { SpaceMission } from "./types";
 
 export const spaceMissionColumnDefs: ColDef<SpaceMission>[] = [
-    { field: "mission" },
+    { field: "mission", filter: true },
     { field: "company" },
     { field: "location" },
     { field: "date" },
