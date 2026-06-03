46a0dde7e5ce446796e6419c7b9878ff4e75d578
REL <rel.yesodot@gmail.com>
2026-06-03 15:02:38 +0300

    refactor: enhance currency formatter to handle non-numeric values in price column

--- src/features/SpaceMission/columns.ts
+++ src/features/SpaceMission/columns.ts
@@ -1,17 +1,25 @@
-import type { ColDef } from "ag-grid-community";
+import type { ColDef, ValueFormatterParams } from "ag-grid-community";
 import type { SpaceMission } from "./types";
 
-const currencyFormatter = (params) => "£" + params.value.toLocaleString();
+const currencyFormatter = ({
+    value,
+}: ValueFormatterParams<SpaceMission, number>) => {
+    if (typeof value !== "number") return "";
+
+    return "£" + value.toLocaleString();
+};
 
 export const spaceMissionColumnDefs: ColDef<SpaceMission>[] = [
-    { field: "mission", filter: false }, // Column Definitions take precedence over Default Column Definitions
+    {
+        field: "mission",
+        filter: false, // Column Definitions take precedence over Default Column Definitions
+    },
     { field: "company" },
     { field: "location" },
     { field: "date" },
     {
         field: "price",
-        // Return a formatted string for this column
-        valueFormatter: currencyFormatter,
+        valueFormatter: currencyFormatter, // Return a formatted string for this column
     },
     { field: "successful" },
     { field: "rocket" },
